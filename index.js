d4857a810664f406cf954a2b6cce5da033bdec7c
Cyprian Tseriwa <h210661a@hit.ac.zw>
2022-09-15 01:22:44 +0200

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+function validate(){
+var name = document.getElementById("name").value;
+var phone = document.getElementById("phone").value;
+var email = document.getElementById("email").value;
+var enquiry = document.getElementById("message").value;
+
+
+}
